a2b5d4f3aefffc9b5dd16dc1b3e75247359b07c5
benlemus <benlemus234@gmail.com>
2024-09-01 03:38:21 -0700

    updates person when incorrect, shows and replaces underscores for word_to_be_guessed as needed, shows letter guessed with counter

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import random
+
 # WORD TO BE GUESSED
-        # HAVE BANK OF WORDS AND INSTEAD OF ENTERING, IT AUTO PICKS RANDOM WORD AND STARTS
-word_to_be_guessed = ['h','e','l','l','o']
-state_of_guessed = ['_','_','_','_','_']
+words = ['hello', 'word', 'kori', 'marvin', 'milo', 'mojo', 'cute', 'mouse']
+
+word_to_be_guessed = []
+state_of_guessed = []
 
 # GUESSED
 letters_guessed = []
+num_incorrect = 0
+
+# GUESSING ACTIVITY
+guessing_active = True
+convert_word = True
 
 # ALPHABET
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
@@ -20,9 +28,11 @@ line5 = ['    |     ']
 line6 = ["    |    "]
 line7 = [" ___|___"]
 
-all_lines = [line1, line2, line3, line4, line5, line6, line7]
 
 def show_game():
+
+    all_lines = [line1, line2, line3, line4, line5, line6, line7]
+
     for line in all_lines:
         for char in line:
             print(char)
@@ -37,11 +47,25 @@ def checks_letter_guessed(guess):
                 return True
                 
             else:
+                global num_incorrect
+                num_incorrect += 1
                 return False
         else:
-            print('LETTER ALREADY GUESSED')
+            print('\n !! LETTER ALREADY GUESSED !!')
     else:
-        print('ENTER VALID LETTER')
+        print('\n !! ENTER VALID LETTER !!')
+
+def get_word_to_be_guessed():
+    length_of_word_bank = len(words)
+
+    random_int = random.randint(1,length_of_word_bank)
+
+    return words[random_int - 1]
+
+def updates_word_to_be_guessed(word):
+    global word_to_be_guessed
+    word_to_be_guessed = list(word)
+    
 
 def updates_word_state(guess):
     indx_of_guess = 0
@@ -51,38 +75,58 @@ def updates_word_state(guess):
        if char == guess: 
         new_indx = indx_of_guess - 1
         state_of_guessed[new_indx] = guess
-        
 
-         
-            
+def shows_word_state():
+    global convert_word
+
+    if convert_word:
+        for char in word_to_be_guessed:
+            state_of_guessed.append('_')
+        convert_word = False
     
+    state_of_guessed_str = ' '.join(state_of_guessed)
+    print(f'Word To Be Guessed: {state_of_guessed_str}')
+        
+def shows_letters_guessed():
+    letters_guessed_to_str = ', '.join(letters_guessed)
+
+    letters_guessed_count = len(letters_guessed)
+
+    print(f'Letters Guessed({letters_guessed_count}): {letters_guessed_to_str}')
+
+def update_board(is_correct):
+    if is_correct == True:
+        return 
+    elif is_correct == False:
+        global num_incorrect
+        a = num_incorrect
+
+        global line3
+        global line4
+        global line5
+        global line6
+
+        if a == 1:
+            line3 = ['    |     O']
+        elif a == 2:
+            line4 = ['    |     |']
+        elif a == 3:
+            line4 = ['    |     |/']
+        elif a == 4:
+            line4 = ['    |    \|/']
+        elif a == 5:
+            line5 = ['    |     |']
+        elif a == 6:
+            line6 = ["    |      \\"]
+        elif a == 7:
+            line6 = ["    |    / \\"]
+
     
 def guess_promt():
-    guess = input('ENTER GUESS: ').lower()
 
-    is_correct = checks_letter_guessed(guess)
-
-    if is_correct:
-        print('correct')
-        updates_word_state(guess)
-       
-    else:
-        print('incorrect')
-
-
-            # calls to show updated game and promts guess again
-        # if not correct, tells user, calls to check/update person, calls to show updated game and promts guess again
-       
-
-def start_game():
-    # show underscores of each letter
-
-    # show game
-    show_game()
-
-    # asks for guess
-    guess_promt()
-
+    guess = input('Enter Guess: ').lower()
+    print('\n\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\\n/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/')
+    return guess
 
 
 # CHECKS FOR PERSON
@@ -115,34 +159,68 @@ def checks_for_legs(line):
             return False
     
 
-# ADDS PERSON
-
-    # HEAD
-def add_head():
-    return ['    |     O']
-
-    # ARMS
-def add_back_arm():
-    return ['    |     |']
-
-def add_right_arm():
-    return ['    |     |/']
-
-def add_full_arms():
-    return ['    |    \|/']
-
-    # BACK
-def add_back():
-    return ['    |     |']
-
-    # LEGS
-def add_right_leg():
-    return ["    |      \\"]
-
-def add_full_legs():
-    return ["    |    / \\"]
-
         
+def main():
+    # GET WORD
+        # updates word_to_be_guessed
+    word = get_word_to_be_guessed()
+
+    updates_word_to_be_guessed(word)
+
+    global guessing_active
+
+    while guessing_active == True:
+        show_game()
+
+        shows_word_state()
+
+        shows_letters_guessed()
+
+        guess = guess_promt()
+      
+        is_correct = checks_letter_guessed(guess)
+
+        update_board(is_correct)
+        
+        if guess == '**':
+            break
+
+        if is_correct:
+            updates_word_state(guess)
+            print('\n| correct |\n')
+        else:
+            print('\n| incorrect |\n')
+
+
+        global num_incorrect
+        if num_incorrect == 7:
+            guessing_active = False
+
+            show_game()
+
+            print('Game Over!!')
+
+            shows_letters_guessed()
+            
+            print('Press ENTER to play again')
+            print('Press Q then ENTER to exit.')
+
+            play_again_promt = input('')
+
+            if play_again_promt == '':
+                guessing_active = True
+                main()
+            if play_again_promt == 'q':
+                pass
+
+        # ADD WHEN YOU WIN
+
+if __name__ == "__main__":
+     main()
+
+
+
+
 
 # line1 = ["     _____"]
 # line1 = ["    |     |"]
@@ -151,5 +229,3 @@ def add_full_legs():
 # line5 = ['    |     |']
 # line5 = ["    |    / \\"]
 # line5 = ["____|____"]
-
-
