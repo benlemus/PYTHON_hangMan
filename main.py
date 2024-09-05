84b12ecd5e11074f531999470a01e0390653074e
benlemus <benlemus234@gmail.com>
2024-09-05 01:19:38 -0700

    added colors and win screen. Fully playable game in terminal

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import random
+from termcolor import colored
 
 # WORD TO BE GUESSED
 words = ['hello', 'word', 'kori', 'marvin', 'milo', 'mojo', 'cute', 'mouse']
 
 word_to_be_guessed = []
 state_of_guessed = []
+no_color_state = []
 
 # GUESSED
 letters_guessed = []
@@ -51,9 +53,9 @@ def checks_letter_guessed(guess):
                 num_incorrect += 1
                 return False
         else:
-            print('\n !! LETTER ALREADY GUESSED !!')
+            print(colored('\n !! LETTER ALREADY GUESSED !!', 'light_blue'))
     else:
-        print('\n !! ENTER VALID LETTER !!')
+        print(colored('\n !! ENTER VALID LETTER !!', 'light_blue'))
 
 def get_word_to_be_guessed():
     length_of_word_bank = len(words)
@@ -68,13 +70,13 @@ def updates_word_to_be_guessed(word):
     
 
 def updates_word_state(guess):
-    indx_of_guess = 0
 
     for char in word_to_be_guessed:
-       indx_of_guess += 1
        if char == guess: 
-        new_indx = indx_of_guess - 1
-        state_of_guessed[new_indx] = guess
+        char_indx = word_to_be_guessed.index(char)
+        state_of_guessed[char_indx] = colored(guess, 'green')
+        no_color_state[char_indx] = guess
+
 
 def shows_word_state():
     global convert_word
@@ -82,6 +84,7 @@ def shows_word_state():
     if convert_word:
         for char in word_to_be_guessed:
             state_of_guessed.append('_')
+            no_color_state.append('_')
         convert_word = False
     
     state_of_guessed_str = ' '.join(state_of_guessed)
@@ -107,25 +110,30 @@ def update_board(is_correct):
         global line6
 
         if a == 1:
-            line3 = ['    |     O']
+            line3 = [f"    |     {colored('O', 'red')}"]
         elif a == 2:
-            line4 = ['    |     |']
+            line4 = [f"    |     {colored('|', 'red')}"]
         elif a == 3:
-            line4 = ['    |     |/']
+            arm = '|/'
+            line4 = [f"    |     {colored(arm, 'red')}"]
         elif a == 4:
-            line4 = ['    |    \|/']
+            double_arms = '\|/'
+            line4 = [f"    |    {colored(double_arms, 'red')}"]
         elif a == 5:
-            line5 = ['    |     |']
+            line5 = [f"    |     {colored('|', 'red')}"]
         elif a == 6:
-            line6 = ["    |      \\"]
+            leg = '\\'
+            line6 = [f"    |      {colored(leg, 'red')}"]
         elif a == 7:
-            line6 = ["    |    / \\"]
+            double_legs = '/ \\'
+            line6 = [f"    |    {colored(double_legs, 'red')}"]
 
     
 def guess_promt():
 
     guess = input('Enter Guess: ').lower()
     print('\n\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\\n/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/')
+    
     return guess
 
 
@@ -157,17 +165,81 @@ def checks_for_legs(line):
             return 'full'
         else:
             return False
+        
+
     
+def end_game():
+    global guessing_active
+    global line1
+    global line2
+    global line3
+    global line4
+    global line5
+    global line6
+    global line7
+    global word_to_be_guessed
+    global state_of_guessed
+    global letters_guessed
+    global num_incorrect
+    global convert_word
+    
+    guessing_active = False
+
+    show_game()
+
+    print(colored('Game Over!!', 'red'))
+
+    shows_letters_guessed()
+            
+    play_again_promt = input(f"Press {colored('ENTER', 'green')} to play again \nPress {colored('Q', 'green')} then {colored('ENTER', 'green')} to exit.")
+    if play_again_promt == '':
+
+        guessing_active = True
+
+        line1 = ["     _____"]
+        line2 = ["    |     |"]
+        line3 = ['    |     ']
+        line4 = ['    |    '] 
+        line5 = ['    |     ']
+        line6 = ["    |    "]
+        line7 = [" ___|___"]
+
+        word_to_be_guessed = []
+        state_of_guessed = []
+
+
+        letters_guessed = []
+        num_incorrect = 0
+
+        convert_word = True
+
+        main()
+    if play_again_promt == 'q':
+        pass
+
+def shows_win():
+    global guessing_active
+
+    guessing_active = False
+
+    show_game()
+
+    shows_word_state()
+
+    shows_letters_guessed()
+
+    print(colored('YOU WIN!!', 'green'))
 
         
 def main():
-    # GET WORD
-        # updates word_to_be_guessed
     word = get_word_to_be_guessed()
 
     updates_word_to_be_guessed(word)
 
     global guessing_active
+    global current_guess
+    global letters_guessed
+    global no_color_state
 
     while guessing_active == True:
         show_game()
@@ -177,41 +249,34 @@ def main():
         shows_letters_guessed()
 
         guess = guess_promt()
+
+        if guess == '**':
+            guessing_active = False
+            break
       
         is_correct = checks_letter_guessed(guess)
 
-        update_board(is_correct)
-        
-        if guess == '**':
-            break
-
         if is_correct:
             updates_word_state(guess)
-            print('\n| correct |\n')
+            char_indx = letters_guessed.index(guess)
+            letters_guessed[char_indx] = colored(guess, 'green')
+            print(colored('\n| correct |\n', 'green'))
         else:
-            print('\n| incorrect |\n')
+            char_indx = letters_guessed.index(guess)
+            letters_guessed[char_indx] = colored(guess, 'red')
+            print(colored('\n| incorrect |\n', 'red'))
 
+        update_board(is_correct)
+        
+        if no_color_state == word_to_be_guessed:
+            shows_win()
+            # print(colored('YOU WIN!!', 'green'))
+            # guessing_active = False
+        
 
         global num_incorrect
         if num_incorrect == 7:
-            guessing_active = False
-
-            show_game()
-
-            print('Game Over!!')
-
-            shows_letters_guessed()
-            
-            print('Press ENTER to play again')
-            print('Press Q then ENTER to exit.')
-
-            play_again_promt = input('')
-
-            if play_again_promt == '':
-                guessing_active = True
-                main()
-            if play_again_promt == 'q':
-                pass
+            end_game()
 
         # ADD WHEN YOU WIN
 
@@ -223,9 +288,9 @@ if __name__ == "__main__":
 
 
 # line1 = ["     _____"]
-# line1 = ["    |     |"]
-# line1 = ['    |     O']
-# line5 = ['    |    \|/']
+# line2 = ["    |     |"]
+# line3 = ['    |     O']
+# line4 = ['    |    \|/']
 # line5 = ['    |     |']
-# line5 = ["    |    / \\"]
-# line5 = ["____|____"]
+# line6 = ["    |    / \\"]
+# line7 = ["____|____"]
